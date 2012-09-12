8f93718080cd38d59b824103a1eabeef57dec8e5
Alistair Jones <alistair.jones@gmail.com>
2012-09-12 14:37:02 +0100

    Change Cart API before wiring in to a window pool.

--- kernel/src/main/java/org/neo4j/kernel/impl/nioneo/store/ScanResistantWindowPoolFactory.java
+++ kernel/src/main/java/org/neo4j/kernel/impl/nioneo/store/ScanResistantWindowPoolFactory.java
@@ -7,10 +7,12 @@ import org.neo4j.kernel.impl.util.StringLogger;
 
 public class ScanResistantWindowPoolFactory implements WindowPoolFactory
 {
+    private int targetBytesPerPage = 4096;
+
     @Override
     public WindowPool create( String storageFileName, int recordSize, FileChannel fileChannel,
                               Config configuration, StringLogger log )
     {
-        return new ScanResistantWindowPool( storageFileName, recordSize, fileChannel );
+        return new ScanResistantWindowPool( storageFileName, recordSize, targetBytesPerPage, fileChannel );
     }
 }
