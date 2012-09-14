831dfd51c131e4bdf522a26ac64884daae2337de
Tobias Lindaaker <tobias@thobe.org>
2012-09-14 15:59:01 +0200

    Added caching for property key and relationship label records when checking.
    Made sure license headers were up to date.

--- kernel/src/test/java/org/neo4j/kernel/impl/nioneo/xa/TransactionWriterTest.java
+++ kernel/src/test/java/org/neo4j/kernel/impl/nioneo/xa/TransactionWriterTest.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2002-2012 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.neo4j.kernel.impl.nioneo.xa;
 
 import java.io.IOException;
@@ -72,6 +91,7 @@ public class TransactionWriterTest
 
     private final Map<Class<?>, Comparison> comparisons = new HashMap<Class<?>, Comparison>();
 
+    @SuppressWarnings("unchecked")
     private <T extends AbstractBaseRecord> Matcher<T> matchesRecord( final T record )
     {
         final Comparison comparison = comparison( record.getClass() );
